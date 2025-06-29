d49c64282891d9757cdc5b34efa5febc4c048ca9
Isu <natantrojak@wp.pl>
2025-06-29 14:15:33 +0200

    Reinitialized repo

---
+++ README.md
@@ -0,0 +1,4 @@
+1. Copy whole dir
+2. Modify files (.json file cannot be named keymap.json)
+3. qmk flash -kb (keyboardName) -km (path to new directory [CWD is keymaps dir itself])
+4. Run the keyboard in flash mode. In case of Corne rev4, mount a keyboard (it works as a drive file)
---
+++ corne41/20250629/keymap.c
@@ -0,0 +1,70 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT_split_3x6_3_ex2(
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, DT_PRNT,                                      DT_UP, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
+    KC_ESC, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G, KC_PSCR,      DT_DOWN, KC_H, RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
+    KC_GRV, KC_Z, KC_X, KC_C, KC_V, KC_B,                                                      KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_DEL, 
+    MO(5), LT(1,KC_SPC), MO(4),                                                         KC_ENT, MO(2), KC_RALT
+  ),
+  [1] = LAYOUT_split_3x6_3_ex2(
+    KC_TRNS, KC_NO, KC_BSLS, KC_ASTR, KC_NO, KC_NO, KC_NO,                              KC_NO, KC_NO, KC_7, KC_8, KC_9, KC_NO, KC_TRNS, 
+    KC_TRNS, KC_LGUI, LALT_T(KC_MINS), LCTL_T(KC_PLUS), LSFT_T(KC_EQL), KC_NO, KC_NO,   KC_NO, KC_NO, RSFT_T(KC_4), RCTL_T(KC_5), LALT_T(KC_6), RGUI_T(KC_UNDS), KC_TRNS, 
+    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                KC_NO, KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS,
+    KC_NO, KC_TRNS, KC_NO,                                                              KC_NO, LT(3,KC_0), KC_NO
+  ),
+  [2] = LAYOUT_split_3x6_3_ex2(
+    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                  KC_NO, KC_NO, KC_APP, KC_NO, KC_NO, KC_NO, KC_TRNS,
+    KC_TRNS, KC_LT, KC_LBRC, KC_LPRN, KC_LCBR, KC_NO, KC_NO,                            KC_NO, KC_NO, KC_RCBR, KC_RPRN, KC_RBRC, KC_GT, KC_PIPE, 
+    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                KC_NO, KC_NO, KC_NO, KC_NO, KC_BSLS, KC_TRNS, 
+    KC_TRNS, MO(3), KC_TRNS,                                                            KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+  [3] = LAYOUT_split_3x6_3_ex2(
+    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                  KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_F12, KC_TRNS, 
+    KC_TRNS, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_NO,                          KC_NO, KC_NO, KC_F4, KC_F5, KC_F6, KC_F11, KC_TRNS, 
+    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                KC_NO, KC_F1, KC_F2, KC_F3, KC_F10, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS,                                                          KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+  [4] = LAYOUT_split_3x6_3_ex2(
+    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                  KC_NO, KC_NO, KC_PGDN, KC_PGUP, KC_NO, KC_NO, KC_TRNS, 
+    KC_TRNS, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_NO,                          KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_TRNS, 
+    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                KC_NO, KC_HOME, KC_END, KC_NO, KC_NO, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS,                                                          KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+  [5] = LAYOUT_split_3x6_3_ex2(
+    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                  KC_NO, KC_NO, KC_AMPR, KC_ASTR, KC_PIPE, KC_NO, KC_TRNS, 
+    KC_TRNS, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_NO,                          KC_NO, KC_NO, KC_DLR, KC_PERC, KC_CIRC, KC_UNDS, KC_TRNS, 
+    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                KC_NO, KC_EXLM, KC_AT, KC_HASH, KC_NO, KC_TRNS, 
+    KC_TRNS, KC_TRNS, KC_TRNS,                                                          KC_MINS, KC_EQL, KC_PLUS
+  ),
+};
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+        switch (keycode) {
+				case RSFT_T(KC_J):
+                case LSFT_T(KC_F): 
+                        return TAPPING_TERM - 100;
+				case LCTL_T(KC_D):
+				case LCTL_T(KC_K):
+						return TAPPING_TERM - 50;
+                default:
+                        return TAPPING_TERM;
+}}
+
